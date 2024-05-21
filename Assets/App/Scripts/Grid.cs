688b86e0ea36a7ba3fe4b6d6d8bd7f7dc15d35cb
Danila Buyakevich <boss.buyakevich@mail.ru>
2024-05-21 17:27:02 +0300

    :/

--- Assets/App/Scripts/Grid.cs
+++ Assets/App/Scripts/Grid.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using CodeMonkey.Utils;
@@ -9,6 +10,7 @@ public class Grid
     private int _height;
     private float _cellSize;
     private int[,] _gridArray;
+    private TextMesh[,] _debugTextArray;
 
     public Grid(int width, int height, float cellSize)
     {
@@ -17,7 +19,8 @@ public class Grid
         _cellSize = cellSize;
 
         _gridArray = new int[_width, _height];
-
+        _debugTextArray = new TextMesh[_width, _height];
+        
         GenerateGrid();
     }
 
@@ -27,9 +30,9 @@ public class Grid
         {
             for (int z = 0; z < _gridArray.GetLength(1); z++)
             {
-                var text = UtilsClass.CreateWorldText(_gridArray[x, z].ToString(), null, GetWorldPosition(x, 0, z) + new Vector3(_cellSize,0,_cellSize) * 0.5f, 30,
+                _debugTextArray[x,z] = UtilsClass.CreateWorldText(_gridArray[x, z].ToString(), null, GetWorldPosition(x, 0, z) + new Vector3(_cellSize,0,_cellSize) * 0.5f, 30,
                     Color.white, TextAnchor.MiddleCenter);
-                text.transform.rotation = Quaternion.Euler(90f, 0f, 0f);
+                _debugTextArray[x,z].transform.rotation = Quaternion.Euler(90f, 0f, 0f);
 
                 Debug.DrawLine(GetWorldPosition(x, 0, z), GetWorldPosition(x, 0, z + 1), Color.white, 100f);
                 Debug.DrawLine(GetWorldPosition(x, 0, z), GetWorldPosition(x + 1, 0, z), Color.white, 100f);
@@ -43,5 +46,46 @@ public class Grid
     private Vector3 GetWorldPosition(int x, int y, int z)
     {
         return new Vector3(x, y, z) * _cellSize;
+    }
+
+    private void GetXZ(Vector3 worldPosition, out int x, out int z)
+    {
+        x = Mathf.FloorToInt(worldPosition.x / _cellSize);
+        z = Mathf.FloorToInt(worldPosition.z / _cellSize);
+    }
+    public void SetValue(int x, int z, int value)
+    {
+        if (x >= 0 && z >= 0 && x < _width && z < _height)
+        {
+            _gridArray[x, z] = value;
+            _debugTextArray[x, z].text = _gridArray[x, z].ToString();
+        }
+    }
+
+    public void SetValue(Vector3 worldPosition, int value)
+    {
+        int x, z;
+        GetXZ(worldPosition, out x, out z);
+        SetValue(x,z,value);
+        Debug.Log("Boink!");
+    }
+
+    public int GetValue(int x, int z)
+    {
+        if (x >= 0 && z >= 0 && x < _width && z < _height)
+        {
+            return _gridArray[x, z];
+        }
+        else
+        {
+            return 0;
+        }
+    }
+    public int GetValue(Vector3 worldPosition)
+    {
+        int x, z;
+        GetXZ(worldPosition,out x,out z);
+        return GetValue(x, z);
+
     }
 }
--- Assets/App/Scripts/Testing.cs
+++ Assets/App/Scripts/Testing.cs
@@ -1,21 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
+using CodeMonkey.Utils;
 using UnityEngine;
 
 public class Testing : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private Grid _grid;
+
     void Start()
     {
-        Grid grid = new Grid(10, 10, 10f);
-        Debug.Log(grid);
+        _grid = new Grid(5, 5, 10f);
+        Debug.Log(_grid);
     }
 
-    
-
-    // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetMouseButtonDown(0))
+        {
+            GetMousePosition();
+
+        }
+
+        if (Input.GetMouseButtonDown(1))
+        {
+            Debug.Log(_grid.GetValue(GetMousePosition()));
+        }
+    }
+
+    private Vector3 GetMousePosition()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out RaycastHit hit))
+        {
+            Vector3 mouseWorldPosition = hit.point;
+            _grid.SetValue(mouseWorldPosition, 228);
+        return mouseWorldPosition;
+        }
+
+        return Vector3.zero;
     }
 }
