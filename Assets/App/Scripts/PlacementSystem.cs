62aad98ddc43c9693ec9b520d53dae65472dc742
Danila Buyakevich <boss.buyakevich@mail.ru>
2024-05-22 20:07:43 +0300

    create collision between objects

--- Assets/App/Scripts/PlacementSystem.cs
+++ Assets/App/Scripts/PlacementSystem.cs
@@ -15,9 +15,18 @@ public class PlacementSystem : MonoBehaviour
 
     [SerializeField] private GameObject gridVisualization;
 
+    private GridData floorData;
+    private GridData furnitureData;
+
+    private Renderer previewRenderer;
+    private List<GameObject> placedGameObject = new();
+
     private void Start()
     {
         StopPlacement();
+        floorData = new GridData();
+        furnitureData = new GridData();
+        previewRenderer = cellIndicator.GetComponentInChildren<Renderer>();
     }
 
     public void StartPlacement(int ID)
@@ -29,6 +38,7 @@ public class PlacementSystem : MonoBehaviour
             Debug.LogError($"No ID found {ID}");
             return;
         }
+
         gridVisualization.SetActive(true);
         cellIndicator.SetActive(true);
         inputManager.OnClicked += PlaceStructure;
@@ -41,10 +51,25 @@ public class PlacementSystem : MonoBehaviour
         {
             return;
         }
+
         Vector3 mousePosition = inputManager.GetSelectedMapPosition();
         Vector3Int gridPosition = grid.WorldToCell(mousePosition);
+
+        bool placementValidity = CheckPlacementValidity(gridPosition, selectedObjectIndex);
+        if (!placementValidity) return;
+
         GameObject newObject = Instantiate(_databaseSo.objectsData[selectedObjectIndex].Prefab);
         newObject.transform.position = grid.CellToWorld(gridPosition);
+        placedGameObject.Add(newObject);
+        GridData selectedData = _databaseSo.objectsData[selectedObjectIndex].ID == 0 ? floorData : furnitureData;
+        selectedData.AddObjectAt(gridPosition, _databaseSo.objectsData[selectedObjectIndex].Size,
+            _databaseSo.objectsData[selectedObjectIndex].ID, placedGameObject.Count - 1);
+    }
+
+    private bool CheckPlacementValidity(Vector3Int gridPosition, int i)
+    {
+        GridData selectedData = _databaseSo.objectsData[selectedObjectIndex].ID == 0 ? floorData : furnitureData;
+        return selectedData.CanPlaceObjectAt(gridPosition, _databaseSo.objectsData[selectedObjectIndex].Size);
     }
 
     private void StopPlacement()
@@ -64,6 +89,10 @@ public class PlacementSystem : MonoBehaviour
         }
         Vector3 mousePosition = inputManager.GetSelectedMapPosition();
         Vector3Int gridPosition = grid.WorldToCell(mousePosition);
+
+        bool placementValidity = CheckPlacementValidity(gridPosition, selectedObjectIndex);
+        previewRenderer.material.color = placementValidity ? Color.white : Color.red;
+        Debug.Log(previewRenderer.material.color);
         mouseIndicator.transform.position = mousePosition;
         cellIndicator.transform.position = grid.CellToWorld(gridPosition);
     }
