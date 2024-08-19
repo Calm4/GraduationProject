61f81daaf42a75e95933687ecc6d383b57912ebf
Danila Buyakevich <boss.buyakevich@mail.ru>
2024-08-20 00:28:53 +0300

    delete useless hide point

--- Assets/App/Scripts/BuildingPanelUI.cs
+++ Assets/App/Scripts/BuildingPanelUI.cs
@@ -7,35 +7,50 @@ namespace App.Scripts
 {
     public class BuildingPanelUI : MonoBehaviour
     {
-        private RectTransform _goRectTransform; 
-        [SerializeField] private RectTransform positionToHidePanel; 
+        private RectTransform _goRectTransform;
     
         [Title("Animation Params")] 
         [SerializeField] private float animationTime;
-    
+
         public event Action<float> OnButtonPressed;
-    
+
         private bool _isHide;
-        private Vector2 _panelStartPosition; 
-    
+        private Vector2 _panelStartPosition;
+
         private void Start()
         {
             _goRectTransform = GetComponent<RectTransform>();
             _panelStartPosition = _goRectTransform.anchoredPosition;
         }
-    
+
         public void ShowOrHideBuildingsPanel()
         {
             OnButtonPressed?.Invoke(animationTime);
+
             if (_isHide)
             {
-                _goRectTransform.DOAnchorPos(_panelStartPosition, animationTime).SetEase(Ease.InOutSine);  
+                // Вернуть панель на исходную позицию
+                _goRectTransform.DOAnchorPos(_panelStartPosition, animationTime).SetEase(Ease.InOutSine);
             }
             else
             {
-                _goRectTransform.DOAnchorPos(positionToHidePanel.anchoredPosition, animationTime).SetEase(Ease.InCirc);  
+                // Рассчитать позицию, при которой панель полностью скрыта за нижней границей экрана
+                Vector2 hidePosition = CalculateHidePosition();
+                _goRectTransform.DOAnchorPos(hidePosition, animationTime).SetEase(Ease.InCirc);
             }
+
             _isHide = !_isHide;
+        }
+
+        private Vector2 CalculateHidePosition()
+        {
+            float panelHeight = _goRectTransform.rect.height;
+            
+            Vector2 hidePosition = _panelStartPosition;
+
+            hidePosition.y = _panelStartPosition.y - panelHeight;
+
+            return hidePosition;
         }
     }
 }
